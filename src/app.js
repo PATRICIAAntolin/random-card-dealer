01843871bcfd960369b4773f8661a663a1c4270e
PATRICIAAntolin <prothings2021@gmail.com>
2021-06-21 20:58:14 +0000

    falta revisar

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suite = ["diamond", "heart", "spade", "club"];
+  const symbol = ["♦", "♥", "♠", "♣"];
+  const number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const numberIndex = Math.floor(Math.random() * 13);
+  const suiteIndex = Math.floor(Math.random() * 4);
+
+  document.querySelector("#app").innerHTML = `
+  <div class="card ${suite[suiteIndex]}">
+  <span>${symbol[suiteIndex]}</span>
+  <p>${number[numberIndex]}</p>
+  <span>${symbol[suiteIndex]}</span>
+  </div>
+  `;
 };
